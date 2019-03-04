457a5524f2674801fc4ab3084c859477a1750f89
Mohamed <qandili92@gmail.com>
2019-03-04 11:37:28 +0100

    TpPoo1 Commit

--- src/com/TpPoo2/Entreprise/EntrepriseTest.java
+++ src/com/TpPoo2/Entreprise/EntrepriseTest.java
@@ -2,10 +2,10 @@ package com.TpPoo2.Entreprise;
 
 public class EntrepriseTest {
     public static void main(String[] args) {
-        Employee producteur=new Producteur("mohamed","elqandili",21,2019,5);
-        Employee vendeur=new Vendeur("test ","test",19,2019,1000);
-        Employee representant=new Representant("ahah ","haha",18,2017,500);
-        Personnel personnel=new Personnel(4);
+        Employee producteur = new Producteur("mohamed", "elqandili", 21, 2019, 5);
+        Employee vendeur = new Vendeur("test ", "test", 19, 2019, 1000);
+        Employee representant = new Representant("ahah ", "haha", 18, 2017, 500);
+        Personnel personnel = new Personnel(4);
         personnel.ajouterEmploye(producteur);
         personnel.ajouterEmploye(vendeur);
         personnel.ajouterEmploye(representant);
